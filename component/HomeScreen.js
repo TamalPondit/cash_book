c88dbe51d4bdc4fd3604cf47108e048436d4be60
TamalPondit <tamalpondit@gmail.com>
2022-11-06 17:36:01 +0600

    first commit

---
+++ component/HomeScreen.js
@@ -0,0 +1,14 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ padding:10, fontSize:20}}>Cash Book Home page</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
